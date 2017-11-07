be9f24cb4d62ee78a4116699451d334ba60f6df1
stojan milosev <stojancina@gmail.com>
2017-11-07 18:40:14 +0100

    added new problems

--- src/includes/json.php
+++ src/includes/json.php
@@ -8,17 +8,17 @@ $mlecni_program = $json['mlecni_program'];
 //kondiktorski_program
 $kondiktorski_program = $json['kondiktorski_program'];
 $katarina_mladenovac = $kondiktorski_program[1]['katarina_mladenovac'];
-$aleksandrija = $kondiktorski_program[1]['aleksandrija'];
-$zeneva_lesnica = $kondiktorski_program[1]['zeneva_lesnica'];
+$aleksandrija = $kondiktorski_program[2]['aleksandrija'];
+$zeneva_lesnica = $kondiktorski_program[3]['zeneva_lesnica'];
 //testenine
 $testenine = $json['testenine'];
 $sentela = $testenine[1]['sentela'];
-$danubis  = $testenine[1]['danubis'];
-$renesansa  = $testenine[1]['renesansa'];
+$danubis  = $testenine[2]['danubis'];
+$renesansa  = $testenine[3]['renesansa'];
 //pakeraj
 $pakeraj = $json['pakeraj'];
 $koktel_sivac = $pakeraj[1]['koktel_sivac'];
-$koktel_sivac = $pakeraj[1]['ares'];
+$koktel_sivac = $pakeraj[2]['ares'];
 //osnovne zivotne namirnice
 $osnovne_zivotne_namirnice = $json['osnovne_zivotne_namirnice'];
 //papirna konfekcija
@@ -26,8 +26,8 @@ $papirna_konfekcija = $json['papirna_konfekcija'];
 //hemija
 $hemija = $json['hemija_tekstil'];
 $fresh = $hemija[1]['fresh'];
-$tisa_becej = $hemija[1]['tisa_becej'];
-$tekstil = $hemija[1]['tekstil'];
+$tisa_becej = $hemija[2]['tisa_becej'];
+$tekstil = $hemija[3]['tekstil'];
 //kese dzakovi za smece
 $kese = $json['kese'];
 $kese_tregerice = $kese[1]['kese_tregerice'];
--- src/js/main.js
+++ src/js/main.js
@@ -20,15 +20,35 @@ $(document).ready(function(){
 
     $('#katarina-mladenovac').click(function (){
        $('.tab-content').removeClass("show");
-        $('##katarina-mladenovac-proizvodi').addClass('show');
+        $('#katarina-mladenovac-proizvodi').addClass('show');
+    });
+	$('#aleksandrija').click(function (){
+       $('.tab-content').removeClass("show");
+        $('#aleksandrija-proizvodi').addClass('show');
+    });
+	$('#zeneva-lesnica').click(function (){
+       $('.tab-content').removeClass("show");
+        $('#zeneva-lesnica-proizvodi').addClass('show');
     });
     $('#sentela').click(function (){
        $('.tab-content').removeClass("show");
         $('#sentela-proizvodi').addClass('show');
     });
-	$('#pakeraj').click(function (){
+    $('#danubis').click(function (){
        $('.tab-content').removeClass("show");
-        $('#pakeraj-proizvodi').addClass('show');
+        $('#danubis-proizvodi').addClass('show');
+    });
+    $('#renesansa').click(function (){
+       $('.tab-content').removeClass("show");
+        $('#renesansa-proizvodi').addClass('show');
+    });
+    $('#koktel-sivac').click(function (){
+       $('.tab-content').removeClass("show");
+        $('#koktel-sivac-proizvodi').addClass('show');
+    });
+	$('#ares').click(function (){
+       $('.tab-content').removeClass("show");
+        $('#ares-proizvodi').addClass('show');
     });
 	$('#osnovne_zivotne_namirnice').click(function (){
        $('.tab-content').removeClass("show");
@@ -42,9 +62,21 @@ $(document).ready(function(){
        $('.tab-content').removeClass("show");
         $('#hemija-fresh-proizvodi').addClass('show');
     });
+	$('#tisa-becej').click(function (){
+       $('.tab-content').removeClass("show");
+        $('#tisa-becej-proizvodi').addClass('show');
+    });
 	$('#kese-tregerice').click(function (){
        $('.tab-content').removeClass("show");
         $('#kese-tregerice-proizvodi').addClass('show');
+    });
+	$('#kese-za-zamrzivac').click(function (){
+       $('.tab-content').removeClass("show");
+        $('#kese-za-zamrzivac-proizvodi').addClass('show');
+    });
+	$('#dzakovi-za-smece').click(function (){
+       $('.tab-content').removeClass("show");
+        $('#dzakovi-za-smece-proizvodi').addClass('show');
     });
 	$('#vino-sokovi').click(function (){
        $('.tab-content').removeClass("show");
